e8f1f56964a41f0df751bd014eeb91563983774a
xugangjava <583904274@qq.com>
2019-07-22 18:42:34 +0800

    xg
    xg

--- pypyhlddz/dllmain.cpp
+++ pypyhlddz/dllmain.cpp
@@ -158,6 +158,77 @@ extern "C" _declspec(dllexport) void hlddz_search_can_out_card(
 
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+
+extern "C" _declspec(dllexport)  void tdlz_search_out_card(
+	const BYTE* usr_card_1,
+	const int  usr_card_count_1,
+	const BYTE* usr_card_2,
+	const int  usr_card_count_2,
+	const BYTE* usr_card_3,
+	const int  usr_card_count_3,
+	const BYTE* desk_card,
+	const int  desk_card_count,
+	const BYTE usr_idx,
+	const BYTE desk_idx,
+	const BYTE banker_idx,
+	const BYTE tlz,		//天癞子
+	const BYTE dlz,		//地癞子
+	tagOutCardResult* result
+) {
+	BYTE all_card_data[4][MAX_COUNT];
+	memcpy(all_card_data[0], usr_card_1, usr_card_count_1);
+	memcpy(all_card_data[1], usr_card_2, usr_card_count_2);
+	memcpy(all_card_data[2], usr_card_3, usr_card_count_3);
+	memcpy(all_card_data[3], desk_card, desk_card_count);
+	game_cards_2_ai_cards(all_card_data[0], usr_card_count_1);
+	game_cards_2_ai_cards(all_card_data[1], usr_card_count_2);
+	game_cards_2_ai_cards(all_card_data[2], usr_card_count_3);
+	game_cards_2_ai_cards(all_card_data[3], desk_card_count);
+	int dsk_card_count = desk_card_count;
+	if (usr_idx == desk_idx) {
+		dsk_card_count = 0;
+	}
+	CGameLogic logic;
+	logic.SetBanker(banker_idx);
+	logic.SetUserCard(0, all_card_data[0], usr_card_count_1);
+	logic.SetUserCard(1, all_card_data[1], usr_card_count_2);
+	logic.SetUserCard(2, all_card_data[2], usr_card_count_3);
+	int usr_card_count = logic.m_cbUserCardCount[usr_idx];
+	logic.SortCardList(all_card_data[3], desk_card_count, ST_ORDER);
+	BYTE lz_card[MAX_COUNT] ;
+	BYTE lz_card_maping[MAX_COUNT] ;
+	ZeroMemory(lz_card, MAX_COUNT);
+	ZeroMemory(lz_card_maping, MAX_COUNT);
+
+	for (int i = 0; i < usr_card_count; i++)
+	{
+		int c = all_card_data[usr_idx][i];
+		int lv = logic.GetCardValue(c);
+		if (lv == tlz || lv == dlz) {
+			lz_card[i] = c;
+			lz_card_maping[i] = lv;
+		}
+	}
+
+	logic.SortCardList(all_card_data[usr_idx], usr_card_count, ST_ORDER);
+	tagOutCardResult rs;
+	logic.SearchOutCard(
+		all_card_data[usr_idx],
+		usr_card_count,
+		all_card_data[3],
+		desk_card_count,
+		desk_idx,//当前出牌最大
+		usr_idx,//我的位置
+		rs);
+	ai_cards_2_game_cards(rs.cbResultCard, rs.cbCardCount);
+	memcpy(result, &rs, sizeof(tagOutCardResult));
+}
+
+
+
+
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
